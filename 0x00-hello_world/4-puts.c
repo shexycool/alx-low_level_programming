f906cd7e12cc91908fd48b56713b11acad4f94c7
shexycool <jhonchristopher071@gmail.com>
2022-12-08 15:30:49 -0800

    my first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@ include <stdio.h>
  */
 int main(void)
 {
-	puts("Programming is like building a multi lingual puzzle\n");
+	puts("\"Programming is like building a multi lingual puzzle\n");
 	return (0);
 }
