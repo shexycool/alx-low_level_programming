85bd3f520d7a9d3bf4fbcdc683ca7a9a0f0fc920
shexycool <jhonchristopher071@gmail.com>
2022-12-08 16:12:31 -0800

    my first code

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+main(void)
+	int main(void)
+{
+	printf("Size of a char: %i bute(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(S)\n", sizeof(int));
+	print("Size of a long int: %i byte(s)\n", sizeof(long Int));
+	print("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	print("Size of a float: %i byte(s)\n". sizeof(float));
+	print("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+        print("Size of a float: %i byte(s)\n". sizeof(float));
+	return(0)
+}
