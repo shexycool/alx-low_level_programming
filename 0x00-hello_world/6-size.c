b1e0d30e59d5ccbe269d842736ce3914b861ab56
shexycool <jhonchristopher071@gmail.com>
2022-12-08 16:23:09 -0800

    my first code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-	printf("Size of a char: %i bute(s)\n", sizeof(char));
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(S)\n", sizeof(int));
-	print("Size of a long int: %i byte(s)\n", sizeof(long Int));
-	print("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-	print("Size of a float: %i byte(s)\n". sizeof(float));
-	print("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-        print("Size of a float: %i byte(s)\n". sizeof(float));
-	return(0)
+	printf("Size of a long int: %i byte(s)\n", sizeof(long Int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+        printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return(0);
 }
