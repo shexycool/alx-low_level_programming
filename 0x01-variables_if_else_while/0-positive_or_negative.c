ef224a380f7aaab0525090f6c24a97cc765d993e
shexycool <jhonchristopher071@gmail.com>
2022-12-09 08:46:16 -0800

    possityve or negatove

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <time.h>
- /**
-  * Entering points
-  * Return: always 0
-  */
+/**
+ * Entering points
+ * Return: always 0
+ */
 int main(void)
- {
-	 int n;
-         rand(time(0));
-         n = rand() - RAND_MAX /2;
-         return (0);
- }
+{
+	int n;
+        rand(time(0));
+        n = rand() - RAND_MAX /2;
+        return (0);
+}
