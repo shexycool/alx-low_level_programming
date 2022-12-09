beed62165e65b20b68df1c0419ceb6c2a06a6595
shexycool <jhonchristopher071@gmail.com>
2022-12-09 08:39:21 -0800

    possityve or negatove

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,8 +6,8 @@
   */
 int main(void)
  {
-         int n;
-         srand(time(0));
-         n = rand() - RAND_MAX / 2;
+	 int n;
+         rand(time(0));
+         n = rand() - RAND_MAX /2;
          return (0);
  }
