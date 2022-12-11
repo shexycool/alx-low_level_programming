4044f63269634dbd964bb89ae3bdb44415c0d68f
shexycool <jhonchristopher071@gmail.com>
2022-12-11 13:03:34 -0800

    my other files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - enter point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	c - 'a';
+	while (c <= '2')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar ('\n');
+	return (0);
+}
