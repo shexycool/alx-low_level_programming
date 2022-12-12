d6fc769edc3f25d0e53d764ef32e3a17089b170a
shexycool <jhonchristopher071@gmail.com>
2022-12-12 12:47:27 -0800

    my files

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int e;
+
+	e = 0;
+	while 
+		(e < 10) {
+			putchar(e + '0');
+			e++;
+		}
+	putchar('\n');
+	return (0);
+}
