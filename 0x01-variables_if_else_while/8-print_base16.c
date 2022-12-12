d5f7e014424e170634c53d42f5173eb638608606
shexycool <jhonchristopher071@gmail.com>
2022-12-12 13:19:57 -0800

    my files

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int s;
+
+	char d;
+
+	d = 'a';
+	while
+		(s <= 10) {
+			putchar(s + '0');
+			s++;
+		}
+	while
+		(d <= 'f') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
