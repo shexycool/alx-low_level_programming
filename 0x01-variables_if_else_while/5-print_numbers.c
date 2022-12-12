515762ec136f0936725b3400658c8be936dbf4a0
shexycool <jhonchristopher071@gmail.com>
2022-12-12 12:07:06 -0800

    my files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entery point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char n = 10;
+
+	if (r == 0; r < n;)
+		putchar(r);
+		r++;
+	putchar('\n');
+	return (0);
+}
