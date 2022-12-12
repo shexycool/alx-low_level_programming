54609a4a8e35f133f4fe831dc5c1a42bc26bac67
shexycool <jhonchristopher071@gmail.com>
2022-12-12 12:56:16 -0800

    my files

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char s;
+
+	s = 'z';
+	while
+		(s >= 'a') {
+			putchar(s);
+			s--;
+		}
+	putchar('\n');
+	return (0);
+}
