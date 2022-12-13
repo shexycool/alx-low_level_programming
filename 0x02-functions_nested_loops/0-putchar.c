228f7fdc9cb93b15e3c5468a5298137c4bd2b980
shexycool <jhonchristopher071@gmail.com>
2022-12-13 13:33:50 -0800

    my putchar file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - starting
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char h;
+
+	h = 0
+	if (h == 0)
+		printf("%d_putchar");
+	else
+		printf("%dnot valid");
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+my nested loops readme
