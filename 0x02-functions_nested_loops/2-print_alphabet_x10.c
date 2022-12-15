0125bdca6276cc3f47a0cf1f92f3bfb600e42bb2
shexycool <jhonchristopher071@gmail.com>
2022-12-14 16:58:26 -0800

    my files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print alphabet_x10 -> print in ten places
+ */
+void print alphabet_x10(void)
+{
+	int e;
+	int s;
+
+	s = 0;
+	e = 'a';
+	do(s < 10; s++) {
+		while (e <=  'z')
+		{
+			_putchar(e);
+			e++;
+		}
+		_putchar('\n');
+	}
+}
