6318498ced325315b4a8c5e64e384e931bf54314
shexycool <jhonchristopher071@gmail.com>
2022-12-15 12:57:03 -0800

    my files absolute value of an interger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs -> to compute the absolute value of an interger
+ * @n: parameter
+ * Return: absolute number value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
