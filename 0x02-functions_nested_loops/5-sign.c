c3631e4d75ff1275bb8a3c549961046d403880fb
shexycool <jhonchristopher071@gmail.com>
2022-12-15 12:42:46 -0800

    my files

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *  print_sign -> print signs
+ *  @n: the argument
+ *  Return: the outputs
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (m == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
