fcc3ae5a2a02a48e97a7fbb4788fcd2b4ac76794
shexycool <jhonchristopher071@gmail.com>
2022-12-14 17:07:22 -0800

    my files

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,11 @@ void print_alphabet_x10(void)
 	int e;
 	int s;
 
-	s = 0;
-	e = 'a';
 	for (s = 0; s < 10; s++)
 	{
-		while (e <=  'z')
+		for (e = 'a'; e <=  'z'; e++)
 		{
 			_putchar(e);
-			e++;
 		}
 		_putchar('\n');
 	}
