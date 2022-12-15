593a00db8fe784e4fee091ba83ab3553ad2a5eb2
shexycool <jhonchristopher071@gmail.com>
2022-12-15 14:39:57 -0800

    my files pringing the times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table -> print times table
+ */
+void times_table(void)
+{
+	int o, b, m;
+
+	for (o = 0; o <= 9; o++)
+	{
+		_putchar('0');
+
+		for (b = 1; b <= 9; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			m = o + b;
+
+			if (m <= 9)
+				_putchar(' ');
+			else
+				_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
