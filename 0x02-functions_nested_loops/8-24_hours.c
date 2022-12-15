2ab18dd7619160e01f7b29a4be14e7a1b558c6be
shexycool <jhonchristopher071@gmail.com>
2022-12-15 14:05:13 -0800

    my files printing last digit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -28,13 +28,13 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(j + '0');
 			}
-			else if (j > 10)
+			else if (j >= 10)
 			{
 				_putchar(':');
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
+			 _putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
