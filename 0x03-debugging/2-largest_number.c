9c4b954b039eb0c35672dbc656e49ce21cf56702
shexycool <jhonchristopher071@gmail.com>
2022-12-17 10:25:26 -0800

    print the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,14 @@
 
 int largest_number(int a, int b, int c)
 {
-	a = 972;
-	b = -98;
-	c = 0;
 
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
