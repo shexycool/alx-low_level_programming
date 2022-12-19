4e2dfabe05f4cefdddabc2d319af24ca9e718d7e
shexycool <jhonchristopher071@gmail.com>
2022-12-19 15:02:16 -0800

    my files on pointers arrays and string projects

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _atoi -> convert string to integer
+ * @s: strint parameter
+ * Return: integer of the string
+ */
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s  - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
+}
